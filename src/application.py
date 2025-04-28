49003a97b49bedfd66345452ea1fe392ac800c82
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2025-04-28 08:08:51 -0300

    criando docker files

--- src/application.py
+++ src/application.py
@@ -1,4 +1,10 @@
 import uvicorn
+import sys
+import os
+
+# Adiciona /app no caminho para o Python encontrar 'src'
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 if __name__ == "__main__":
-    uvicorn.run("mcp_server:app", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run("src.mcp_server:app", host="0.0.0.0", port=3000, reload=True)
